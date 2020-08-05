50402b1141a1353689d1f03491337c9b2179752a
letrungnghia145 <nghia1k45@gmail.com>
2020-08-05 23:47:46 +0700

    fix project set up to deploy

--- src/test/java/api/application/ApplicationTests.java
+++ src/test/java/api/application/ApplicationTests.java
@@ -1,10 +1,10 @@
-package api.appication;
+package api.application;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjectCnpmApiApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
